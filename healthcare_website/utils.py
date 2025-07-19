83fb2757e9b6331f47ab91b38c458008ef764866
suhasmodi <91365423+suhasmodi@users.noreply.github.com>
2025-07-19 17:39:53 +0530

    Update utils.py

--- healthcare_website/utils.py
+++ healthcare_website/utils.py
@@ -1,82 +1,100 @@
 import os
-import numpy as np
+import ast
 import pandas as pd
+import numpy as np
+import joblib
+import logging
 from django.conf import settings
-from tensorflow import keras
+from fuzzywuzzy import process
 from sklearn.preprocessing import LabelEncoder
+import tensorflow as tf
+from tensorflow import keras
 
-# List of all known symptoms
+logger = logging.getLogger(__name__)
+
+# Load all static data at startup
+BASE_PATH = os.path.join(settings.BASE_DIR, "healthcare_website")
+description = pd.read_csv(os.path.join(BASE_PATH, "symptom_Description.csv"))
+precautions = pd.read_csv(os.path.join(BASE_PATH, "symptom_precaution.csv"))
+medications = pd.read_csv(os.path.join(BASE_PATH, "symptom_medication.csv"))
+diets = pd.read_csv(os.path.join(BASE_PATH, "symptom_diet.csv"))
+doc_vs_dis = pd.read_csv(os.path.join(BASE_PATH, "doctors_dataset.csv"))
+
+# Symptom list used for fuzzy matching
 symptoms = [
-    "itching", "skin_rash", "nodal_skin_eruptions", "continuous_sneezing", "shivering", "chills",
-    "joint_pain", "stomach_pain", "acidity", "ulcers_on_tongue", "muscle_wasting", "vomiting",
-    "burning_micturition", "spotting_urination", "fatigue", "weight_gain", "anxiety",
-    "cold_hands_and_feets", "mood_swings", "weight_loss", "restlessness", "lethargy",
-    "patches_in_throat", "irregular_sugar_level", "cough", "high_fever", "sunken_eyes",
-    "breathlessness", "sweating", "dehydration", "indigestion", "headache", "yellowish_skin",
-    "dark_urine", "nausea", "loss_of_appetite", "pain_behind_the_eyes", "back_pain",
-    "constipation", "abdominal_pain", "diarrhoea", "mild_fever", "yellow_urine", "yellowing_of_eyes",
-    "acute_liver_failure", "fluid_overload", "swelling_of_stomach", "swelled_lymph_nodes",
-    "malaise", "blurred_and_distorted_vision", "phlegm", "throat_irritation", "redness_of_eyes",
-    "sinus_pressure", "runny_nose", "congestion", "chest_pain", "weakness_in_limbs",
-    "fast_heart_rate", "pain_during_bowel_movements", "pain_in_anal_region", "bloody_stool",
-    "irritation_in_anus", "neck_pain", "dizziness", "cramps", "bruising", "obesity",
-    "swollen_legs", "swollen_blood_vessels", "puffy_face_and_eyes", "enlarged_thyroid",
-    "brittle_nails", "swollen_extremeties", "excessive_hunger", "extra_marital_contacts",
-    "drying_and_tingling_lips", "slurred_speech", "knee_pain", "hip_joint_pain", "muscle_weakness",
-    "stiff_neck", "swelling_joints", "movement_stiffness", "spinning_movements", "loss_of_balance",
-    "unsteadiness", "weakness_of_one_body_side", "loss_of_smell", "bladder_discomfort",
-    "foul_smell_of urine", "continuous_feel_of_urine", "passage_of_gases", "internal_itching",
-    "toxic_look_(typhos)", "depression", "irritability", "muscle_pain", "altered_sensorium",
-    "red_spots_over_body", "belly_pain", "abnormal_menstruation", "dischromic_patches",
-    "watering_from_eyes", "increased_appetite", "polyuria", "family_history", "mucoid_sputum",
-    "rusty_sputum", "lack_of_concentration", "visual_disturbances", "receiving_blood_transfusion",
-    "receiving_unsterile_injections", "coma", "stomach_bleeding", "distention_of_abdomen",
-    "history_of_alcohol_consumption", "fluid_overload.1", "blood_in_sputum", "prominent_veins_on_calf",
-    "palpitations", "painful_walking", "pus_filled_pimples", "blackheads", "scurring",
-    "skin_peeling", "silver_like_dusting", "small_dents_in_nails", "inflammatory_nails",
-    "blister", "red_sore_around_nose", "yellow_crust_ooze"
+    'abdominal_pain', 'abnormal_menstruation', 'acidity', 'acute_liver_failure',
+    'altered_sensorium', 'anxiety', 'back_pain', 'belly_pain', 'blackheads',
+    'bladder_discomfort', 'blister', 'blood_in_sputum', 'bloody_stool', 'blurred_and_distorted_vision',
+    'breathlessness', 'brittle_nails', 'bruising', 'burning_micturition', 'chest_pain',
+    'chills', 'cold_hands_and_feets', 'coma', 'congestion', 'constipation', 'continuous_feel_of_urine',
+    'continuous_sneezing', 'cough', 'cramps', 'dark_urine', 'dehydration', 'depression',
+    'diarrhoea', 'dischromic_patches', 'distention_of_abdomen', 'dizziness', 'drying_and_tingling_lips',
+    'enlarged_thyroid', 'excessive_hunger', 'extra_marital_contacts', 'family_history',
+    'fast_heart_rate', 'fatigue', 'fluid_overload', 'foul_smell_of_urine', 'headache',
+    'high_fever', 'hip_joint_pain', 'history_of_alcohol_consumption', 'increased_appetite',
+    'indigestion', 'inflammatory_nails', 'internal_itching', 'irregular_sugar_level',
+    'irritability', 'irritation_in_anus', 'joint_pain', 'knee_pain', 'lack_of_concentration',
+    'lethargy', 'loss_of_appetite', 'loss_of_balance', 'loss_of_smell', 'malaise',
+    'mild_fever', 'mood_swings', 'movement_stiffness', 'mucoid_sputum', 'muscle_pain',
+    'muscle_wasting', 'muscle_weakness', 'nausea', 'neck_pain', 'nodal_skin_eruptions',
+    'obesity', 'pain_behind_the_eyes', 'pain_during_bowel_movements', 'pain_in_anal_region',
+    'painful_walking', 'palpitations', 'passage_of_gases', 'patches_in_throat',
+    'phlegm', 'polyuria', 'prominent_veins_on_calf', 'puffy_face_and_eyes',
+    'pus_filled_pimples', 'receiving_blood_transfusion', 'receiving_unsterile_injections',
+    'red_sore_around_nose', 'red_spots_over_body', 'redness_of_eyes', 'restlessness',
+    'runny_nose', 'rusty_sputum', 'scurring', 'shivering', 'silver_like_dusting',
+    'sinus_pressure', 'skin_peeling', 'skin_rash', 'slurred_speech', 'small_dents_in_nails',
+    'spinning_movements', 'spotting_urination', 'stiff_neck', 'stomach_bleeding',
+    'stomach_pain', 'sunken_eyes', 'sweating', 'swelled_lymph_nodes', 'swelling_joints',
+    'swelling_of_stomach', 'swollen_blood_vessels', 'swollen_extremeties', 'swollen_legs',
+    'throat_irritation', 'toxic_look_(typhos)', 'ulcers_on_tongue', 'unsteadiness',
+    'visual_disturbances', 'vomiting', 'watering_from_eyes', 'weakness_in_limbs',
+    'weakness_of_one_body_side', 'weight_gain', 'weight_loss', 'yellow_crust_ooze',
+    'yellow_urine', 'yellowing_of_eyes', 'yellowish_skin'
 ]
 
-# Label encoder and disease list
-le = LabelEncoder()
-diseases = [
-    "Fungal infection", "Allergy", "GERD", "Chronic cholestasis", "Drug Reaction", "Peptic ulcer disease",
-    "AIDS", "Diabetes", "Gastroenteritis", "Bronchial Asthma", "Hypertension", "Migraine", "Cervical spondylosis",
-    "Paralysis (brain hemorrhage)", "Jaundice", "Malaria", "Chicken pox", "Dengue", "Typhoid",
-    "Hepatitis A", "Hepatitis B", "Hepatitis C", "Hepatitis D", "Hepatitis E", "Alcoholic hepatitis",
-    "Tuberculosis", "Common Cold", "Pneumonia", "Dimorphic hemmorhoids (piles)", "Heart attack",
-    "Varicose veins", "Hypothyroidism", "Hyperthyroidism", "Hypoglycemia", "Osteoarthristis",
-    "Arthritis", "(vertigo) Paroymsal Positional Vertigo", "Acne", "Urinary tract infection",
-    "Psoriasis", "Impetigo"
-]
-le.fit(diseases)
-
-# Cache the model globally
+# Lazy loading the trained model
 _model = None
-
-def get_model():
+def load_model():
     global _model
     if _model is None:
-        model_dir = os.path.join(settings.BASE_DIR, "healthcare_website")
-        model_path = os.path.join(model_dir, "my_neural_network_model.h5")
+        model_path = os.path.join(BASE_PATH, "my_neural_network_model.h5")
         _model = keras.models.load_model(model_path)
     return _model
 
-def predict_disease(user_input):
-    # Create zeroed DataFrame for all symptoms
-    input_data = pd.DataFrame(np.zeros((1, len(symptoms))), columns=symptoms)
+def predict_disease(user_symptoms):
+    with open(os.path.join(BASE_PATH, "training_data.txt"), "r") as file:
+        data = ast.literal_eval(file.read())
 
-    # Set symptoms present in user input to 1
-    for symptom in user_input:
-        if symptom in input_data.columns:
-            input_data[symptom] = 1
+    all_symptoms = list(data.keys())
+    input_vector = [0] * len(all_symptoms)
+
+    for symptom in user_symptoms:
+        if symptom in all_symptoms:
+            input_vector[all_symptoms.index(symptom)] = 1
         else:
-            print(f"Warning: Symptom '{symptom}' not recognized. Skipping.")
+            logger.warning(f"Symptom '{symptom}' not found in training data.")
 
-    # Load model and predict
-    model = get_model()
-    prediction_probabilities = model.predict(input_data)
-    predicted_index = np.argmax(prediction_probabilities)
-    predicted_disease = le.inverse_transform([predicted_index])[0]
+    model = load_model()
+    input_array = np.array([input_vector])
+    prediction = model.predict(input_array)[0]
 
-    return str(predicted_disease)
+    predicted_index = np.argmax(prediction)
+    encoder = joblib.load(os.path.join(BASE_PATH, "label_encoder.joblib"))
+    predicted_disease = encoder.inverse_transform([predicted_index])[0]
+
+    return predicted_disease
+
+def get_disease_information(disease):
+    info = {
+        'description': description[description['Disease'].str.lower() == disease.lower()]['Description'].values[0],
+        'precautions': precautions[precautions['Disease'].str.lower() == disease.lower()]['Precaution'].values[0].split(','),
+        'medications': medications[medications['Disease'].str.lower() == disease.lower()]['Medication'].values[0].split(','),
+        'diets': diets[diets['Disease'].str.lower() == disease.lower()]['Diet'].values[0].split(','),
+        'doctor': doc_vs_dis[doc_vs_dis['Disease'].str.lower() == disease.lower()]['Specialist'].values[0]
+    }
+    return info
+
+def correct_symptom_name(input_symptom):
+    match, score = process.extractOne(input_symptom, symptoms)
+    return match if score > 60 else input_symptom
